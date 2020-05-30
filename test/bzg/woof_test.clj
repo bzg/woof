c98ee00c543ffb85d4cb214ab574778cf8eb9482
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-05-30 15:04:15 +0200

    Add preliminary code for tests

---
+++ test/bzg/woof_test.clj
@@ -0,0 +1,46 @@
+(ns bzg.woof-test
+  (:require [bzg.core :as core]
+            [bzg.config :as config]
+            [clojure.test :refer :all]))
+
+;; Confirm a bug
+(def msg1 {:id        "id1"
+           :subject   "Subject message id1"
+           :from      '({:address (:user config/config)})
+           :date-sent #inst "2020-05-27T00:13:11.037044Z"
+           :headers   [{"X-Original-To" (:mailing-list config/config)}
+                       {"X-Woof-Bug" "confirmed"}]})
+
+(def msg2 {:id        "id2"
+           :subject   "Subject message id2"
+           :from      '({:address (:user config/config)})
+           :date-sent #inst "2020-05-27T00:13:11.037044Z"
+           :headers   [{"X-Original-To" (:mailing-list config/config)}
+                       {"References" "id1"}
+                       {"X-Woof-Bug" "fixed"}]})
+
+(def msg3 {:id        "id3"
+           :subject   "Release 8.2"
+           :from      '({:address (:user config/config)})
+           :date-sent #inst "2020-05-27T00:13:11.037044Z"
+           :headers   [{"X-Original-To" (:mailing-list config/config)}
+                       {"X-Woof-Release" "8.2"}]})
+
+(def msg4 {:id        "id4"
+           :subject   "Release 8.3"
+           :from      '({:address (:user config/config)})
+           :date-sent #inst "2020-05-27T00:13:11.037044Z"
+           :headers   [{"X-Original-To" (:mailing-list config/config)}
+                       {"X-Woof-Release" "8.3"}]})
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+;; (process-incoming-message msg1)
+;; (process-incoming-message msg2)
+;; (process-incoming-message msg3)
+;; @db
+;; (get-unfixed-bugs @db)
+;; (get-releases @db)
+;; (get-unfixed-bugs @db)
