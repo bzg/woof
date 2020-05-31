17f93d54ac8cb8826643ccda030821463f0fe972
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-05-31 08:21:38 +0200

    src/bzg/feeds.clj: Refactoring

--- src/bzg/feeds.clj
+++ src/bzg/feeds.clj
@@ -3,92 +3,64 @@
             [bzg.config :as config]
             [bzg.web :as web]
             [bzg.core :as core]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [hiccup.page :as h]))
+
+(defn feed-description [msg type]
+  (let [cdata "<![CDATA[ %s ]]>"]
+    (format cdata (h/html5 [:body (web/format-link-fn msg type)]))))
+
+(defn feed-item [{:keys [id subject date from] :as msg} type]
+  {:title       subject
+   :link        (format (:mail-url-format config/config) id)
+   :description (feed-description msg type)
+   :author      from
+   :pubDate     date})
+
+(defn feed [path items]
+  {:status 200
+   :body
+   (rss/channel-xml
+    {:title       (str (:feed-title config/config) " - " path)
+     :link        (string/replace
+                   (:base-url config/config)
+                   #"([^/])/*$" (str "$1" path))
+     :description (str (:feed-description config/config) " - " path)}
+    items)})
 
 (defn feed-updates [_]
-  (letfn [(format-item [{:keys [id subject date from]}]
-            {:title       subject
-             :link        (format (:mail-url-format config/config) id)
-             :description subject
-             :author      from
-             :pubDate     date})]
-    {:status 200
-     :body
-     (rss/channel-xml
-      {:title       (:feed-title config/config)
-       :link        (string/replace
-                     (:base-url config/config)
-                     #"([^/])/*$" "$1/feed/updates")
-       :description (:feed-description config/config)}
-      (sort-by
-       :pubDate
-       (concat
-        (map format-item
-             (web/intern-id (core/get-unfixed-bugs @core/db)))
-        (map format-item
-             (web/intern-id (core/get-unreleased-changes @core/db)))
-        (map format-item
-             (web/intern-id (core/get-releases @core/db))))))}))
+  (feed "/feed/updates"
+        (sort-by
+         :pubDate
+         (concat
+          (map #(feed-item % :bug)
+               (web/intern-id (core/get-unfixed-bugs @core/db)))
+          (map #(feed-item % :change)
+               (web/intern-id (core/get-unreleased-changes @core/db)))
+          (map #(feed-item % :release)
+               (web/intern-id (core/get-releases @core/db)))))))
 
 (defn feed-bugs [_]
-  (letfn [(format-item [{:keys [id subject date from]}]
-            {:title       subject
-             :link        (format (:mail-url-format config/config) id)
-             :description subject
-             :author      from
-             :pubDate     date})]
-    {:status 200
-     :body
-     (rss/channel-xml
-      {:title       (:feed-title config/config)
-       :link        (string/replace
-                     (:base-url config/config)
-                     #"([^/])/*$" "$1/feed/bugs")
-       :description (:feed-description config/config)}
-      (sort-by
-       :pubDate
-       (concat
-        (map format-item
-             (web/intern-id (core/get-unfixed-bugs @core/db))))))}))
-
-(defn feed-releases [_]
-  (letfn [(format-item [{:keys [id subject date from]}]
-            {:title       subject
-             :link        (format (:mail-url-format config/config) id)
-             :description subject
-             :author      from
-             :pubDate     date})]
-    {:status 200
-     :body
-     (rss/channel-xml
-      {:title       (:feed-title config/config)
-       :link        (string/replace
-                     (:base-url config/config)
-                     #"([^/])/*$" "$1/feed/releases")
-       :description (:feed-description config/config)}
-      (sort-by
-       :pubDate
-       (concat
-        (map format-item
-             (web/intern-id (core/get-releases @core/db))))))}))
+  (feed "/feed/bugs"
+        (sort-by
+         :pubDate
+         (concat
+          (map #(feed-item % :bug)
+               (web/intern-id (core/get-unfixed-bugs @core/db)))))))
 
 (defn feed-changes [_]
-  (letfn [(format-item [{:keys [id subject date from]}]
-            {:title       subject
-             :link        (format (:mail-url-format config/config) id)
-             :description subject
-             :author      from
-             :pubDate     date})]
-    {:status 200
-     :body
-     (rss/channel-xml
-      {:title       (:feed-title config/config)
-       :link        (string/replace
-                     (:base-url config/config)
-                     #"([^/])/*$" "$1/feed/changes")
-       :description (:feed-description config/config)}
-      (sort-by
-       :pubDate
-       (concat
-        (map format-item
-             (web/intern-id (core/get-unreleased-changes @core/db))))))}))
+  (feed "/feed/changes"
+        (sort-by
+         :pubDate
+         (concat
+          (map #(feed-item % :change)
+               (web/intern-id (core/get-unreleased-changes @core/db)))))))
+
+(defn feed-releases [_]
+  (feed "/feed/releases"
+        (sort-by
+         :pubDate
+         (concat
+          (map #(feed-item % :release)
+               (web/intern-id (core/get-releases @core/db)))))))
+
